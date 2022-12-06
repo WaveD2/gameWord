183a6eb8fa535400a80361580b5cbc1c8b0666b3
waved <tungwaved@gmail.com>
2022-12-06 08:45:32 +0700

    update login

--- index.js
+++ index.js
@@ -50,23 +50,409 @@
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 })();
-// (function () {
-//   const audio = new Audio();
-//   // audio.src = "./music.mp3.mp3";
-//   audio.play();
-// })();
-// -------------
-import { words } from "./words.js";
+
+// ---------------------------------
+function handleSlideTym() {
+  var settings = {
+    particles: {
+      length: 500, // maximum amount of particles
+
+      duration: 2, // particle duration in sec
+
+      velocity: 100, // particle velocity in pixels/sec
+
+      effect: -0.75, // play with this for a nice effect
+
+      size: 30, // particle size in pixels
+    },
+  };
+
+  /*
+
+      * RequestAnimationFrame polyfill by Erik Möller
+
+      */
+
+  (function () {
+    var b = 0;
+    var c = ["ms", "moz", "webkit", "o"];
+    for (var a = 0; a < c.length && !window.requestAnimationFrame; ++a) {
+      window.requestAnimationFrame = window[c[a] + "RequestAnimationFrame"];
+      window.cancelAnimationFrame =
+        window[c[a] + "CancelAnimationFrame"] ||
+        window[c[a] + "CancelRequestAnimationFrame"];
+    }
+    if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function (h, e) {
+        var d = new Date().getTime();
+        var f = Math.max(0, 16 - (d - b));
+        var g = window.setTimeout(function () {
+          h(d + f);
+        }, f);
+        b = d + f;
+        return g;
+      };
+    }
+    if (!window.cancelAnimationFrame) {
+      window.cancelAnimationFrame = function (d) {
+        clearTimeout(d);
+      };
+    }
+  })();
+
+  /*
+
+      * Point class
+
+      */
+
+  var Point = (function () {
+    function Point(x, y) {
+      this.x = typeof x !== "undefined" ? x : 0;
+
+      this.y = typeof y !== "undefined" ? y : 0;
+    }
+
+    Point.prototype.clone = function () {
+      return new Point(this.x, this.y);
+    };
+
+    Point.prototype.length = function (length) {
+      if (typeof length == "undefined")
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+
+      this.normalize();
+
+      this.x *= length;
+
+      this.y *= length;
+
+      return this;
+    };
+
+    Point.prototype.normalize = function () {
+      var length = this.length();
+
+      this.x /= length;
+
+      this.y /= length;
+
+      return this;
+    };
+
+    return Point;
+  })();
+
+  /*
+
+      * Particle class
+
+      */
+
+  var Particle = (function () {
+    function Particle() {
+      this.position = new Point();
+
+      this.velocity = new Point();
+
+      this.acceleration = new Point();
+
+      this.age = 0;
+    }
+
+    Particle.prototype.initialize = function (x, y, dx, dy) {
+      this.position.x = x;
+
+      this.position.y = y;
+
+      this.velocity.x = dx;
+
+      this.velocity.y = dy;
+
+      this.acceleration.x = dx * settings.particles.effect;
+
+      this.acceleration.y = dy * settings.particles.effect;
+
+      this.age = 0;
+    };
+
+    Particle.prototype.update = function (deltaTime) {
+      this.position.x += this.velocity.x * deltaTime;
+
+      this.position.y += this.velocity.y * deltaTime;
+
+      this.velocity.x += this.acceleration.x * deltaTime;
+
+      this.velocity.y += this.acceleration.y * deltaTime;
+
+      this.age += deltaTime;
+    };
+
+    Particle.prototype.draw = function (context, image) {
+      function ease(t) {
+        return -t * t * t + 1;
+      }
+
+      var size = image.width * ease(this.age / settings.particles.duration);
+
+      context.globalAlpha = 1 - this.age / settings.particles.duration;
+
+      context.drawImage(
+        image,
+        this.position.x - size / 2,
+        this.position.y - size / 2,
+        size,
+        size
+      );
+    };
+
+    return Particle;
+  })();
+
+  /*
+
+      * ParticlePool class
+
+      */
+
+  var ParticlePool = (function () {
+    var particles,
+      firstActive = 0,
+      firstFree = 0,
+      duration = settings.particles.duration;
+
+    function ParticlePool(length) {
+      // create and populate particle pool
+
+      particles = new Array(length);
+
+      for (var i = 0; i < particles.length; i++) particles[i] = new Particle();
+    }
+
+    ParticlePool.prototype.add = function (x, y, dx, dy) {
+      particles[firstFree].initialize(x, y, dx, dy);
+
+      // handle circular queue
+
+      firstFree++;
+
+      if (firstFree == particles.length) firstFree = 0;
+
+      if (firstActive == firstFree) firstActive++;
+
+      if (firstActive == particles.length) firstActive = 0;
+    };
+
+    ParticlePool.prototype.update = function (deltaTime) {
+      var i;
+
+      // update active particles
+
+      if (firstActive < firstFree) {
+        for (i = firstActive; i < firstFree; i++)
+          particles[i].update(deltaTime);
+      }
+
+      if (firstFree < firstActive) {
+        for (i = firstActive; i < particles.length; i++)
+          particles[i].update(deltaTime);
+
+        for (i = 0; i < firstFree; i++) particles[i].update(deltaTime);
+      }
+
+      // remove inactive particles
+
+      while (
+        particles[firstActive].age >= duration &&
+        firstActive != firstFree
+      ) {
+        firstActive++;
+
+        if (firstActive == particles.length) firstActive = 0;
+      }
+    };
+
+    ParticlePool.prototype.draw = function (context, image) {
+      // draw active particles
+
+      if (firstActive < firstFree) {
+        for (var i = firstActive; i < firstFree; i++)
+          particles[i].draw(context, image);
+      }
+
+      if (firstFree < firstActive) {
+        for (var i = firstActive; i < particles.length; i++)
+          particles[i].draw(context, image);
+
+        for (i = 0; i < firstFree; i++) particles[i].draw(context, image);
+      }
+    };
+
+    return ParticlePool;
+  })();
+
+  /*
+
+      * Putting it all together
+
+      */
+
+  (function (canvas) {
+    var context = canvas.getContext("2d"),
+      particles = new ParticlePool(settings.particles.length),
+      particleRate = settings.particles.length / settings.particles.duration, // particles/sec
+      time;
+
+    // get point on heart with -PI <= t <= PI
+
+    function pointOnHeart(t) {
+      return new Point(
+        160 * Math.pow(Math.sin(t), 3),
+
+        130 * Math.cos(t) -
+          50 * Math.cos(2 * t) -
+          20 * Math.cos(3 * t) -
+          10 * Math.cos(4 * t) +
+          25
+      );
+    }
+
+    // creating the particle image using a dummy canvas
+
+    var image = (function () {
+      var canvas = document.createElement("canvas"),
+        context = canvas.getContext("2d");
+
+      canvas.width = settings.particles.size;
+
+      canvas.height = settings.particles.size;
+
+      // helper function to create the path
+
+      function to(t) {
+        var point = pointOnHeart(t);
+
+        point.x =
+          settings.particles.size / 2 +
+          (point.x * settings.particles.size) / 350;
+
+        point.y =
+          settings.particles.size / 2 -
+          (point.y * settings.particles.size) / 350;
+
+        return point;
+      }
+
+      // create the path
+
+      context.beginPath();
+
+      var t = -Math.PI;
+
+      var point = to(t);
+
+      context.moveTo(point.x, point.y);
+
+      while (t < Math.PI) {
+        t += 0.01; // baby steps!
+
+        point = to(t);
+
+        context.lineTo(point.x, point.y);
+      }
+
+      context.closePath();
+
+      // create the fill
+      // #ea80b0
+      context.fillStyle = "#df2b2b";
+      context.fill();
+
+      // create the image
+
+      var image = new Image();
+
+      image.src = canvas.toDataURL();
+
+      return image;
+    })();
+
+    // render that thing!
+
+    function render() {
+      // next animation frame
+
+      requestAnimationFrame(render);
+
+      // update time
+
+      var newTime = new Date().getTime() / 1000,
+        deltaTime = newTime - (time || newTime);
+
+      time = newTime;
+
+      // clear canvas
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // create new particles
+
+      var amount = particleRate * deltaTime;
+
+      for (var i = 0; i < amount; i++) {
+        var pos = pointOnHeart(Math.PI - 2 * Math.PI * Math.random());
+
+        var dir = pos.clone().length(settings.particles.velocity);
+
+        particles.add(
+          canvas.width / 2 + pos.x,
+          canvas.height / 2 - pos.y,
+          dir.x,
+          -dir.y
+        );
+      }
+
+      // update and draw particles
+
+      particles.update(deltaTime);
+
+      particles.draw(context, image);
+    }
+
+    // handle (re-)sizing of the canvas
+
+    function onResize() {
+      canvas.width = canvas.clientWidth;
+
+      canvas.height = canvas.clientHeight;
+    }
+
+    window.onresize = onResize;
+
+    // delay rendering bootstrap
+
+    setTimeout(function () {
+      onResize();
+
+      render();
+    }, 10);
+  })(document.getElementById("pinkboard"));
+}
+
+// ---------------------
+import { words, nameUser } from "./words.js";
 const wordText = document.querySelector(".word");
 const questionText = document.querySelector(".question"),
   resultWord = document.querySelector(".result"),
   input = document.querySelector("input"),
   audioMusic = document.querySelector(".audio_music"),
+  audioMusicIcon = document.querySelectorAll(".audio_music  i"),
   form = document.querySelector("form");
 
 const timeText = document.querySelector(".time"),
   checkWord = document.querySelector(".check-word"),
   start = document.querySelector(".start"),
+  textQuestion = document.querySelector(".text"),
   hint = document.querySelector(".hint"),
   contents = document.querySelector(".contents"),
   container = document.querySelector(".container"),
@@ -74,17 +460,29 @@ const timeText = document.querySelector(".time"),
   containerContent = document.querySelector(".container-content"),
   boxDragon = document.querySelector(".box_Dragon"),
   btnDragon = document.querySelector(".btn-dragon"),
+  loginUser = document.querySelector(".login_user"),
+  tym = document.querySelector(".tym"),
+  btnLogin = document.querySelector(".btn-login"),
+  btnTym = document.querySelector(".btn-tym"),
+  valueInputLogin = document.querySelector(".login_user input"),
   btnMarry = document.querySelector(".btn_marry");
 
 audioMusic.addEventListener("click", () => handleMusic());
 let a = false;
 const audio = new Audio();
-
 const handleMusic = () => {
   a = !a;
-  console.log(a);
   audio.src = "./music.mp3.mp3";
-  a ? audio.play() : audio.pause();
+  console.log(audioMusicIcon);
+  if (a) {
+    audioMusicIcon[0].classList.add("none");
+    audioMusicIcon[1].classList.remove("none");
+    audio.play();
+  } else {
+    audioMusicIcon[0].classList.remove("none");
+    audioMusicIcon[1].classList.add("none");
+    audio.pause();
+  }
 };
 btnMarry.addEventListener("click", () => {
   contents.classList.add("none");
@@ -96,15 +494,19 @@ checkWord.addEventListener(
   "click",
   () => (
     (checkWord.style.pointerEvent = "none"),
-    input.value ? handleCheck() : alert("không được spam kiểm tra đáp án")
+    inputWords.value ? handleCheck() : alert("không được spam kiểm tra đáp án")
   )
 );
+btnLogin.addEventListener("click", () => handleSubmit());
 btnDragon.addEventListener("click", () => handleDragon());
+btnTym.addEventListener("click", () => handleTym());
 let index = 0;
 // start
 function handleStart() {
   setTime();
   appWord();
+
+  textQuestion.classList.remove("none");
   inputWords.classList.remove("none");
   start.classList.add("none");
   checkWord.classList.remove("none");
@@ -115,11 +517,12 @@ function handleDragon() {
 }
 function handleCheck() {
   checkWord.style.pointerEvents = "none";
-  let inputValue = input.value.trim().toLowerCase().replace(/\s/g, "");
+  let inputValue = inputWords.value.trim().toLowerCase().replace(/\s/g, "");
   let wordValue = words[index - 1].word
     .trim()
     .toLocaleLowerCase()
     .replace(/\s/g, "");
+  console.log(wordValue, inputValue);
   if (wordValue === inputValue) {
     console.log("đúng");
     wordText.innerText = words[index - 1].word.trim().toUpperCase();
@@ -131,22 +534,45 @@ function handleCheck() {
       wordText.innerText = "";
       questionText.innerText = words[index - 1].question;
       resultWord.innerText = "";
-      input.value = "";
+      inputWords.value = "";
       checkWord.style.pointerEvents = "visible";
     }, 3000);
   } else {
     resultWord.innerText = "Sai rồi !!";
+    checkWord.style.pointerEvents = "visible";
+  }
+}
+function handleSubmit() {
+  if (
+    valueInputLogin.value.trim().length > 3 ||
+    Number.isNaN(valueInputLogin.value.trim())
+  ) {
+    loginUser.classList.add("none");
+    contents.classList.remove("none");
+  } else if (valueInputLogin.value.trim() === "") {
+    alert("Bạn chưa nhập tên");
+  } else {
+    alert("bạn nhập sai tên");
   }
 }
 function appWord() {
-  hint.innerText = `Câu hỏi ${index + 1}:`;
+  hint.innerText = `Câu ${index + 1}:`;
   if (index < words.length) {
     questionText.innerText = words[index].question;
     wordText.innerText = "";
     resultWord.innerText = "";
   } else if (index === words.length) {
-    container.classList.add("none");
-    boxDragon.classList.remove("none");
+    if (
+      valueInputLogin.value.trim().toLowerCase() ===
+      nameUser.name.trim().toLowerCase()
+    ) {
+      handleSlideTym();
+      container.classList.add("none");
+      tym.classList.remove("none");
+    } else {
+      container.classList.add("none");
+      boxDragon.classList.remove("none");
+    }
   }
   index++;
 }
@@ -163,6 +589,10 @@ function setTime() {
   }, 1000);
 
   return time;
+}
+function handleTym() {
+  tym.classList.add("none");
+  boxDragon.classList.remove("none");
 }
 ("use strict");
 
@@ -311,3 +741,5 @@ function windowResized() {
   ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, width, height);
 }
+
+//
--- words.js
+++ words.js
@@ -7,30 +7,33 @@ export const words = [
     word: "Ngọc trai",
     question: "Con trai có gì quý nhất?",
   },
-  {
-    word: "Thâm Tình",
-    question: "Thích Nhung và bị đánh tím mặt gọi là gì",
-  },
-  {
-    word: " Bàn chân",
-    question: "Cái gì mà đi thì nằm, đứng cũng nằm, nhưng nằm lại đứng?",
-  },
-  {
-    word: "12 tháng",
-    question:
-      "Mỗi năm có 7 tháng 31 ngày. Đố bạn có bao nhiêu tháng có 28 ngày?",
-  },
-  {
-    word: "cái áo",
-    question: "Có cổ nhưng không có miệng là gì?",
-  },
-  {
-    word: "Mở mắt",
-    question: "Bạn làm việc gì đầu tiên mỗi buổi sáng?",
-  },
-  {
-    word: "con tem",
-    question:
-      "Tôi chu du khắp thế giới mà tôi vẫn ở nguyên một chỗ, tôi là ai?",
-  },
+  // {
+  //   word: "Thâm Tình",
+  //   question: "Thích Nhung và bị đánh tím mặt gọi là gì",
+  // },
+  // {
+  //   word: " Bàn chân",
+  //   question: "Cái gì mà đi thì nằm, đứng cũng nằm, nhưng nằm lại đứng?",
+  // },
+  // {
+  //   word: "12 tháng",
+  //   question:
+  //     "Mỗi năm có 7 tháng 31 ngày. Đố bạn có bao nhiêu tháng có 28 ngày?",
+  // },
+  // {
+  //   word: "cái áo",
+  //   question: "Có cổ nhưng không có miệng là gì?",
+  // },
+  // {
+  //   word: "Mở mắt",
+  //   question: "Bạn làm việc gì đầu tiên mỗi buổi sáng?",
+  // },
+  // {
+  //   word: "con tem",
+  //   question:
+  //     "Tôi chu du khắp thế giới mà tôi vẫn ở nguyên một chỗ, tôi là ai?",
+  // },
 ];
+export const nameUser = {
+  name: "Hồng Nhung",
+};
